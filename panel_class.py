ea1212327b9007c9cec8e4f77b949b1a5abe88d3
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-21 13:20:07 +0200

    moved isolate class

--- panel_class.py
+++ panel_class.py
@@ -8,28 +8,6 @@ Date: 20/4-23
 from collections import Counter
 
 
-class Isolate:
-    def __init__(self, name: str, antibiotics: list, sir_mic: list):
-        self.name = name
-        self.mic = {
-            antibiotic: (
-                (MIC_value, SIR_category) for MIC_value, SIR_category in sir_mic
-            )
-            for antibiotic in antibiotics
-        }
-
-        def __repr__(self):
-            return self.name
-
-    def get_name(self):
-        return self.name
-
-    def get_mic(self, antibiotic_name=None):
-        if antibiotic_name:
-            return self.mic[antibiotic_name]
-        return self.mic
-
-
 class Panel:
     def __init__(
         self,
