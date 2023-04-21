0d9177c49c38dc873c61f32aa62ac6232b6dbcf4
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-21 09:14:04 +0200

    moved Isolate class out of Panel class

--- panel_class.py
+++ panel_class.py
@@ -7,27 +7,28 @@ Date: 20/4-23
 """
 
 
+class Isolate:
+    def __init__(self, name: str, antibiotics: list, sir_mic: list):
+        self.name = name
+        self.mic = {
+            antibiotics[i]: (sir_mic[i][0], sir_mic[i][1])
+            for i in range(len(antibiotics))
+        }
+
+    def get_name(self):
+        return self.name
+
+    def get_mic(self, antibiotic_name=None):
+        if antibiotic_name:
+            return self.mic[antibiotic_name]
+        return self.mic
+
+
 class Panel:
-    class Isolate:
-        def __init__(self, name: str, antibiotics: list, sir_mic: list):
-            self.name = name
-            self.mic = {
-                antibiotics[i]: (sir_mic[i][0], sir_mic[i][1])
-                for i in range(len(antibiotics))
-            }
-
-        def get_name(self):
-            return self.name
-
-        def get_mic(self, antibiotic_name = None):
-            if antibiotic_name:
-                return self.mic[antibiotic_name]
-            return self.mic
-
     def __init__(
         self,
         available_isolates: list,
-        antibiotics: list, 
+        antibiotics: list,
         chosen_isolates: list = [],
         hyperparameters: list = [1, 1, 1, 1],
     ):
@@ -44,10 +45,11 @@ class Panel:
         antibiotic_mic = {}
         for antibiotic in self.antibiotics:
             antibiotic_mic[antibiotic] = [
-                isolate.get_mic(antibiotic) for isolate in self.chosen_isolates if None not in isolate.get_mic(antibiotic)
-                ]
+                isolate.get_mic(antibiotic)
+                for isolate in self.chosen_isolates
+                if None not in isolate.get_mic(antibiotic)
+            ]
         return antibiotic_mic
-
 
     def spread_score(self):
         pass
@@ -65,7 +67,7 @@ class Panel:
         pass
 
         # calculate redundancyundance score based on self.chosen_isolates
-        #eturn redundant_mics/total_mics
+        # eturn redundant_mics/total_mics
 
     def add_isolate(self):
 
@@ -103,4 +105,3 @@ class Panel:
         self.coverage = self.coverage_score()
         self.redundancy = self.redundancy_score()
         self.antibiotic_mic = self.create_antibiotic_mic()
-
