e22cc9b440ff657d4c4f99218bbd3d97dbfbbc1e
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-21 13:19:49 +0200

    extraced isolates

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pandas as pd
 from panel_class import Panel, Isolate
+from data_extraction_functions import *
 
 
 def main():
-    chosen_isolates_list = pd.read_csv("Chosen_isolates_list.csv")
+    chosen_isolates_csv = pd.read_csv("Chosen_isolates_list.csv")
     CIB = pd.ExcelFile("Q-linea_files\CIB_TF-data_AllIsolates_20230302.xlsx")
     matrix_EU = pd.read_excel(CIB, "matrix EU")
-    antibiotics = list(chosen_isolates_list.columns[3:])
+    antibiotics = list(chosen_isolates_csv.columns[3:])
+
+    # Extract isolates in chosen isolates csv file from excel
+    chosen_isolates = extract_chosen_isolates(chosen_isolates_csv, matrix_EU)
+
+    isolates = extract_isolate_SIR_data(chosen_isolates, antibiotics)
 
 
 if __name__ == "__main__":
