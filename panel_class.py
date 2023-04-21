d4fcc9d592aabbb494527807ad7339474cc9b44a
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-21 08:24:41 +0200

    removed abbreviations of variable names

--- panel_class.py
+++ panel_class.py
@@ -8,60 +8,71 @@ Date: 20/4-23
 
 
 class Panel:
-
     class Isolate:
         def __init__(self, name, antibiotics, sir_mic):
             self.name = name
-            self.mic = {antibiotics[i]:(sir_mic[i][0], sir_mic[i][1]) for i in range(len(antibiotics))}
+            self.mic = {
+                antibiotics[i]: (sir_mic[i][0], sir_mic[i][1])
+                for i in range(len(antibiotics))
+            }
 
-    def __init__(self, ava_iso, chosen_iso = [], a = [1,1,1,1]):
-        self.ava_iso = ava_iso
-        self.chosen_iso = chosen_iso
-        self.spr = self.spr_score()
-        self.cov = self.cov_score()
-        self.red = self.red_score()
-        self.hyppar = a #a list with hyperparameters to be used when constructing panel
+    def __init__(
+        self, available_isolates, chosen_isolates=[], hyperparameters=[1, 1, 1, 1]
+    ):
+        self.available_isolates = available_isolates
+        self.chosen_isolates = chosen_isolates
+        self.spread = self.spread_score()
+        self.coverage = self.coverage_score()
+        self.redundancy = self.redundancy_score()
+        self.hyperparameters = hyperparameters  # a list with hyperparameters to be used when constructing panel
 
-    def spr_score(self):
+    def spread_score(self):
         pass
-        #calculate spread score based on self.chosen_iso
-        #return score
+        # calculate spreadead score based on self.chosen_isolates
+        # return score
 
-    def cov_score(self):
+    def coverage_score(self):
         pass
-        #calculate coverage score based on self.chosen_iso
-        #return score
+        # calculate coverageerage score based on self.chosen_isolates
+        # return score
 
-    def red_score(self):
+    def redundancy_score(self):
         pass
-        #calculate redundance score based on self.chosen_iso
-        #return score
+        # calculate redundancyundance score based on self.chosen_isolates
+        # return score
 
     def add_isolate(self):
 
-        #Find isolate which gives minimum cost
-        iso_cost = {}
-        for isolate in self.ava_iso:
-            iso_cost[isolate] = try_isolate(isolate)
+        # Find isolate which gives minimum cost
+        isolates_cost = {}
+        for isolate in self.available_isolates:
+            isolates_cost[isolate] = try_isolate(isolate)
             remove_isolate(isolate)
 
         def try_isolate(isolate):
-            #Add isolates and calculate temporary scores
-            self.chosen_iso.append(isolate)
-            temp_scores = [-self.spr_score(), -self.cov_score(), self.red_score(), len(self.chosen_iso)]
-            #Calculate the overall cost
-            cost = sum([i*j for (i,j) in zip(self.hyppar,temp_scores)])
+            # Add isolates and calculate temporary scores
+            self.chosen_isolates.append(isolate)
+            temp_scores = [
+                -self.spread_score(),
+                -self.coverage_score(),
+                self.redundancy_score(),
+                len(self.chosen_isolates),
+            ]
+            # Calculate the overall cost
+            cost = sum([i * j for (i, j) in zip(self.hyperparameters, temp_scores)])
             return cost
 
         def remove_isolate(isolate):
-            self.chosen_iso.remove(isolate)
-        
-        #Add isolate
-        best_iso = min(iso_cost, key=iso_cost.get) #gives the key with the maximum value
-        self.chosen_iso.append(best_iso)
-        self.ava_iso.remove(best_iso)
+            self.chosen_isolates.remove(isolate)
 
-        #Update all scores
-        self.spr = self.spr_score()
-        self.cov = self.cov_score()
-        self.red = self.red_score()
+        # Add isolate
+        best_iso = min(
+            isolates_cost, key=isolates_cost.get
+        )  # gives the key with the maximum value
+        self.chosen_isolates.append(best_iso)
+        self.available_isolates.remove(best_iso)
+
+        # Update all scores
+        self.spread = self.spread_score()
+        self.coverage = self.coverage_score()
+        self.redundancy = self.redundancy_score()
