cfa3c9c1a06647b49ac9d22771953bb4005b8171
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-24 18:36:47 +0200

    added doc-strings

--- extract_and_parse.py
+++ extract_and_parse.py
@@ -1,10 +1,18 @@
+"""
+Functions to parse antibiotic data from the Excel file and to extract
+the data wanted in tuples. 
+"""
+
+
 def parse_antibiotic_data(data: str):
+    """Checks if the data is 'nip' or Missing BP'. Used in"""
     if data == "nip" or data.startswith("Missing BP"):
         return False
     return True
 
 
 def extract_antibiotic_data(data: str):
+    """Returns MIC value and SIR category. If off-scale, set MIC value to None"""
     SIR_category = data[0]
     if "<" in data or ">" in data:
         return (None, SIR_category)
