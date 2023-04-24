eabf45a1e8c6cc7069e8f7236cd8c35bae467679
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-24 14:49:22 +0200

    added comments

--- main.py
+++ main.py
@@ -7,22 +7,38 @@ from add_isolate_functions import add_isolate
 
 
 def main():
+    # Read in data from excel
     CIB = pd.ExcelFile("Q-linea_files\CIB_TF-data_AllIsolates_20230302.xlsx")
     matrix_EU = pd.read_excel(CIB, "matrix EU")
-    antibiotics = list(matrix_EU.columns[3:])
 
+    # Create tuple of antibiotic names
+    antibiotics = tuple(matrix_EU.columns[3:])
+
+    # Initiate variables
     panel = Panel()
-    n = 50
-    hyperparameters = np.ones((4, 1))
-
     all_isolates = []
+    n = 50
+    spread_score_coeff = 1
+    coverage_score_coeff = 1
+    redundancy_score_coeff = 1
+    number_of_isolates_coeff = 0.01
+    hyperparameters = np.array(
+        (
+            spread_score_coeff,
+            coverage_score_coeff,
+            redundancy_score_coeff,
+            number_of_isolates_coeff,
+        )
+    )
 
-    for index, row in matrix_EU.iterrows():
+    for _, row in matrix_EU.iterrows():
         isolate, antibiotic_data = row[0], list(row[3:].items())
         isolate_data = {}
+        # Store the MIC value and SIR data for all antibiotics that are not nip or missing bp
         for antibiotic, data in zip(antibiotics, antibiotic_data):
             if parse_antibiotic_data(data):
-                isolate_data[isolate] = extract_antibiotic_data(data)
+                isolate_data[antibiotic] = extract_antibiotic_data(data)
+        # Add an isolate object to all_isolates with the current isolate name and isolate_data
         all_isolates.append(Isolate(isolate, isolate_data))
 
     n = 50
