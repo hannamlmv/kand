25ec5470ad6862f37f791409c08c3fcc5f831ac7
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-24 13:34:46 +0200

    spread

--- panel_class.py
+++ panel_class.py
@@ -27,8 +27,8 @@ class Panel:
         self.antibiotic_mic = self.create_antibiotic_mic()
 
     def create_antibiotic_mic(self):
-        """" Gives a dictionary containing the mic-values present 
-            on the panel for each antibiotic """
+        """ Gives a dictionary containing the mic-values present
+        on the panel for each antibiotic"""
         antibiotic_mic = {}
         for antibiotic in self.antibiotics:
             antibiotic_mic[antibiotic] = [
@@ -39,30 +39,89 @@ class Panel:
         return antibiotic_mic
 
     def spread_score(self):
-        """" Calculates the spread score """
+        """ " Calculates the spread score"""
+        
+        total_concentration_range = [
+            "Min C",
+            0.00195,
+            0.00391,
+            0.00781,
+            0.01563,
+            0.03125,
+            0.0625,
+            0.125,
+            0.25,
+            0.5,
+            1.0,
+            2.0,
+            4.0,
+            8.0,
+            16.0,
+            32.0,
+            64.0,
+            128.0,
+            256.0,
+            512.0,
+            1024.0,
+            "Max C",
+        ]
+
+        def count_gap_length(valid_list: list) -> float:
+            total_gap_length = 0
+            gap_length = 0
+
+            for value in valid_list:
+                if value == 0:
+                    gap_length += 1
+                else:  # if current value == 1
+                    if gap_length >= 2:
+                        total_gap_length += gap_length - 1
+                    gap_length = 0
+            # If the last values were also 0s and the gap length is at least 2.
+            # Add the length of the gap to the total gap length
+            if gap_length >= 2:
+                total_gap_length += gap_length - 1
+            return total_gap_length
+
+
+        def check_edges(valid_list: list) -> float:
+            edge_penalty = 0
+
+            # Add 0.5 penalty if the edge values are empty.
+            if valid_list[0] == 0:  # Check first value
+                edge_penalty += 0.5
+            if valid_list[-1] == 0:  # Check last value
+                edge_penalty += 0.5
+
+            return edge_penalty
+        spread_scores = []
+        for antibiotic in self.antibiotics:
+            mic_values = self.antibiotic_mic[antibiotic]
+            for i in mic_values:
+                
         pass
         # return score
 
     def coverage_score(self):
-        """" Calculates the coverage score """
+        """ " Calculates the coverage score"""
         coverage_scores = []
         for antibiotic in self.antibiotics:
             number_of_mics = len(self.antibiotic_mic[antibiotic])
-            coverage = 0.2*number_of_mics
+            coverage = 0.2 * number_of_mics
             if coverage > 1:
                 coverage = 1
             sir_coverage = 1
-            for category, penalty in {'S':0.3, 'I':0.2, 'R':0.4}.items():
+            for category, penalty in {"S": 0.3, "I": 0.2, "R": 0.4}.items():
                 if category not in [
-                    self.antibiotic_mic[antibiotic][i][1] for i in 
-                    range(len(self.antibiotic_mic[antibiotic]))
-                    ]:
+                    self.antibiotic_mic[antibiotic][i][1]
+                    for i in range(len(self.antibiotic_mic[antibiotic]))
+                ]:
                     sir_coverage -= penalty
             coverage_scores.append(coverage * sir_coverage)
         return sum(coverage_scores) / len(coverage_scores)
 
     def redundancy_score(self):
-        """" Calculates the redundancy score """
+        """ " Calculates the redundancy score"""
         total_mics = 0
         redundant_mics = 0
         for antibiotic in self.antibiotics:
@@ -80,14 +139,14 @@ class Panel:
             return redundant_mics / total_mics
 
     def add_isolate(self):
-        """" Adds one isolate to give the optimal panel """
+        """ " Adds one isolate to give the optimal panel"""
         # Find isolate which gives minimum cost
         isolates_cost = {}
         for isolate in self.available_isolates:
             isolates_cost[isolate] = try_isolate(isolate)
 
         def try_isolate(isolate):
-            """" Investigates which isolate gives the optimal panel """
+            """ " Investigates which isolate gives the optimal panel"""
             # Add isolates and calculate temporary scores
             self.chosen_isolates.append(isolate)
             temp_scores = [
@@ -102,9 +161,7 @@ class Panel:
             return cost
 
         # Add isolate
-        best_isolate = min(
-            isolates_cost, key=isolates_cost.get
-        )
+        best_isolate = min(isolates_cost, key=isolates_cost.get)
         self.chosen_isolates.append(best_isolate)
         self.available_isolates.remove(best_isolate)
 
