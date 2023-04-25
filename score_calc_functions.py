941581fcabc6e2796c5eaf0ecdf84ae0a8f8ecf3
Hanna Malmvall <hanna.malmvall@gmail.com>
2023-04-25 09:11:04 +0200

    la till "över-funktion"

--- score_calc_functions.py
+++ score_calc_functions.py
@@ -1,13 +1,16 @@
 from panel_class import Panel
 
 
-def calc_spread_score(panel: Panel):
+def calc_scores(panel: Panel):
     pass
 
+    def calc_spread_score(panel: Panel):
+        pass
 
-def calc_coverage_score(panel: Panel):
-    pass
+
+    def calc_coverage_score(panel: Panel):
+        pass
 
 
-def calc_redundancy_score(panel: Panel):
-    pass
+    def calc_redundancy_score(panel: Panel):
+        pass
