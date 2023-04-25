3b3bb74a3f80cf3e01f359de1a144761f561a90c
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-25 11:19:05 +0200

    Functions that are used to calculate the spread in a list. None is off-scale interval, 1 means that the MIC value is present and 0 means that it is missing

---
+++ spread_score_functions.py
@@ -0,0 +1,45 @@
+""" Functions used to calculate the spread in a list. Used in score_calc_functions"""
+
+def count_gap_length(valid_list: list) -> float:
+    total_gap_length = 0
+    gap_length = 0
+
+    for value in valid_list:
+        if value == 0:
+            gap_length += 1
+        else:  # if current value == 1
+            if gap_length >= 2:
+                total_gap_length += gap_length - 1
+            gap_length = 0
+    # If the last values were also 0s and the gap length is at least 2.
+    # Add the length of the gap to the total gap length
+    if gap_length >= 2:
+        total_gap_length += gap_length - 1
+    return total_gap_length
+
+
+def check_edges(valid_list: list) -> float:
+    edge_penalty = 0
+
+    # Add 0.5 penalty if the edge values are empty.
+    if valid_list[0] == 0:  # Check first value
+        edge_penalty += 0.5
+    if valid_list[-1] == 0:  # Check last value
+        edge_penalty += 0.5
+
+    return edge_penalty
+
+
+def score_spread_list(spread_list: list) -> float:
+    valid_list = [i for i in spread_list if i is not None]
+
+    if len(valid_list) <= 1:
+        raise ValueError("Length of valid list must be greater than 1")
+
+    penalty = 0
+    penalty += count_gap_length(valid_list)
+    penalty += check_edges(valid_list)
+
+    score = 1 - penalty / len(valid_list)
+    
+    return score
