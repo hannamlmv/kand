f46f8fdac2245cd9be5de34237181521468ce31d
Hanna Malmvall <hanna.malmvall@gmail.com>
2023-04-25 09:38:40 +0200

    skrivit extract_panel_data

--- score_calc_functions.py
+++ score_calc_functions.py
@@ -2,15 +2,33 @@ from panel_class import Panel
 
 
 def calc_scores(panel: Panel):
-    pass
+    antibiotic_mic = extract_panel_data(panel)
+    scores = (
+        calc_spread_score(antibiotic_mic), 
+        calc_coverage_score(antibiotic_mic), 
+        calc_redundancy_score(antibiotic_mic)
+        )
 
-    def calc_spread_score(panel: Panel):
+    def extract_panel_data(panel:Panel):
+        panel_data = {}
+        for isolate in panel.get_chosen_isolates():
+            for abx, (MIC, SIR) in isolate.get_data().items():
+                if abx not in panel_data:
+                    panel_data[abx] = [(MIC, SIR)]
+                else:
+                    panel_data[abx].append((MIC, SIR))
+        return panel_data
+
+    def calc_spread_score(panel_data:dict):
         pass
 
 
-    def calc_coverage_score(panel: Panel):
+    def calc_coverage_score(panel_data:dict):
         pass
 
 
-    def calc_redundancy_score(panel: Panel):
+    def calc_redundancy_score(panel_data:dict):
         pass
+
+    
+    return scores
