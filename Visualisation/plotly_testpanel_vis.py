42460bd58935725a37d460334eeafbdf677bd29c
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-25 14:17:29 +0200

    Fixed zero division errors and handling of antibiotics with Min_C and Max_C as lower or upper limit

---
+++ Visualisation/plotly_testpanel_vis.py
@@ -0,0 +1,302 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from data_extraction_functions import (
+    extract_chosen_isolates,
+    extract_mic_data,
+    extract_SIR,
+    filter_mic_values,
+    parse_on_off_scale,
+    parse_fastidious,
+)
+
+
+def create_plot_df(
+    antibiotics: list, mic_data: list, x_jitter: float = 0.15, y_jitter: float = 0.05
+) -> pd.DataFrame:
+    """Create dataframe used for plotting"""
+
+    mic_dict = {"S": "Sensitive", "I": "Intermediate", "R": "Resistant"}
+
+    fastidious_dict = {
+        "Non-fastidious": [
+            "Staphylococcus epidermidis",
+            "Staphylococcus hominis",
+            "Staphylococcus capitis",
+            "Staphylococcus haemolyticus",
+            "Staphylococcus warneri",
+            "Staphylococcus auricularis",
+            "Staphylococcus aureus",
+            "Staphylococcus lugdunensis",
+            "Enterococcus faecium",
+            "Enterococcus faecalis",
+        ],
+        "Fastidious": [
+            "Streptococcus pneumoniae",
+            "Streptococcus mitis",
+            "Streptococcus oralis",
+            "Streptococcus anginosus",
+            "Streptococcus salivarius",
+            "Streptococcus gallolyticus",
+            "Streptococcus parasanguinis",
+            "Streptococcus constellatus",
+            "Streptococcus gordonii",
+            "Streptococcus intermedius",
+            "Streptococcus mutans",
+            "Streptococcus pyogenes",
+            "Streptococcus agalactiae",
+            "Streptococcus dysgalactiae",
+            "Streptococcus sanguinis",
+        ],
+    }
+
+    # Set ticks of x axis
+    x_axis = [i for i in range(len(antibiotics))]
+
+    # Initialize lists to hold values used for plotting
+    x_values, y_values = [], []
+    SIR_category_list = []
+    MIC_value_list = []
+    isolate_names = []
+    on_off_scale = []
+    pathogen_list = []
+    fastidious_list = []
+
+    for x_value, abx_mic_data in zip(x_axis, mic_data):
+        for isolate, mic_value, SIR_category, scale, pathogen in abx_mic_data:
+            # Add random noise to avoid overlapping
+            x_value_jitter = x_value + np.random.uniform(-x_jitter, x_jitter)
+            mic_value_jitter = mic_value + np.random.uniform(-y_jitter, y_jitter)
+            # Add data to the lists
+            isolate_names.append(isolate)
+            x_values.append(x_value_jitter)
+            SIR_category_list.append(mic_dict[SIR_category])
+            MIC_value_list.append(2 ** (mic_value))
+            on_off_scale.append(scale)
+            pathogen_list.append(pathogen)
+            parse_on_off_scale(scale, SIR_category, y_values, mic_value_jitter)
+            print(isolate)
+            parse_fastidious(fastidious_dict, pathogen, fastidious_list)
+
+    # Create a DF used for plotting
+    plot_df = pd.DataFrame(
+        {
+            "Antibiotics": x_values,
+            "Log2(MIC-value)": y_values,
+            "Isolate names": isolate_names,
+            "SIR": SIR_category_list,
+            "Scale": on_off_scale,
+            "Pathogen": pathogen_list,
+            "MIC value": MIC_value_list,
+            "Fastidiousness": fastidious_list,
+        },
+        index=np.arange(len(x_values)),
+    )
+
+    return plot_df
+
+
+def add_rectangles_to_plot(fig, antibiotics: list) -> None:
+    names_to_conc = {
+        "Benzylpenicillin": ["0.015 - 32.0", "0.008 - 16.0"],
+        "Ampicillin": ["0.125 - 64.0", "0.008 - 16.0"],
+        "Cefoxitin": ["0.25 - 16.0"],
+        "Ceftaroline": ["0.03125 - 16.0"],
+        "Ceftobiprole": ["0.03125 - 16.0"],
+        "Ceftriaxone": ["0.0078125 - 16.0"],
+        "Imipenem": ["0.03125 - 16.0"],
+        "Meropenem": ["0.0078125 - 8.0"],
+        "Ciprofloxacin": ["0.0625 - 8.0"],
+        "Levofloxacin": ["0.03125 - 16.0"],
+        "Gentamicin": ["128.0 - 500.0"],
+        "Dalbavancin": ["0.015 - 1.0"],
+        "Teicoplanin": ["0.03125 - 16.0", "0.03125 - 8.0"],
+        "Vancomycin": ["0.125 - 64.0", "0.015 - 8.0"],
+        "Erythromycin": ["0.015 - 16.0", "0.004 - 4.0"],
+        "Clindamycin": ["0.015 - 16.0", "0.0078125 - 16.0"],
+        "Tetracycline": ["0.015 - 32.0", "0.015 - 16.0"],
+        "Linezolid": ["0.125 - 16.0", "0.0625 - 8.0"],
+        "Daptomycin": ["0.03125 - 16.0"],
+        "Rifampicin": ["0.002 - 8.0"],
+        "Trimeth-sulf": ["0.0625 - 16.0", "0.0625 - 8.0"],
+    }
+
+    for i in range(len(antibiotics)):
+        if antibiotics[i] in names_to_conc:
+            conc = names_to_conc[antibiotics[i]][0].split("-")
+            conc_low = float(conc[0])
+            conc_high = float(conc[1])
+
+            box_witdh = 0.4
+            fig.add_vrect(
+                x0=i - box_witdh,
+                x1=i + box_witdh,
+                y0=0.99 - (10 - np.log2(conc_low / 4)) / 23,
+                y1=1.01 - (10 - np.log2(conc_high / 4)) / 23,
+                fillcolor="skyblue",
+                layer="below",
+                line_width=0,
+                opacity=0.8,
+            )
+
+            if len(names_to_conc[antibiotics[i]]) > 1:
+                conc = names_to_conc[antibiotics[i]][1].split("-")
+                conc_low = float(conc[0])
+                conc_high = float(conc[1])
+
+                box_witdh = 0.3
+                fig.add_vrect(
+                    x0=i - box_witdh,
+                    x1=i + box_witdh,
+                    y0=0.99 - (10 - np.log2(conc_low / 4)) / 23,
+                    y1=1.01 - (10 - np.log2(conc_high / 4)) / 23,
+                    fillcolor="ghostwhite",
+                    layer="below",
+                    line_width=0,
+                    opacity=0.3,
+                )
+
+    fig.add_vrect(
+        x0=10,
+        x1=16,
+        y0=0.97,
+        y1=1,
+        fillcolor="skyblue",
+        line_width=0,
+        opacity=0.8,
+        annotation_text="Non-fastidious concentration ranges",
+        annotation_position="top",
+    )
+    fig.add_vrect(
+        x0=16.5,
+        x1=22.5,
+        y0=0.97,
+        y1=1,
+        fillcolor="ghostwhite",
+        line_width=0,
+        opacity=0.3,
+        annotation_text="Fastidious concentration ranges",
+        annotation_position="top",
+    )
+
+
+def plotly_dotplot(plot_df: pd.DataFrame, antibiotics: list) -> None:
+
+    # Set ticks of x axis
+    x_axis = [i for i in range(len(antibiotics))]
+
+    y_axis_ticktext = [
+        "Min C",
+        "0.00195",
+        "0.00391",
+        "0.00781",
+        "0.01563",
+        "0.03125",
+        "0.0625",
+        "0.125",
+        "0.25",
+        "0.5",
+        "1",
+        "2",
+        "4",
+        "8",
+        "16",
+        "32",
+        "64",
+        "128",
+        "256",
+        "512",
+        "1024",
+        "Max C",
+    ]
+
+    # plot
+    fig = px.scatter(
+        plot_df,
+        x="Antibiotics",
+        y="Log2(MIC-value)",
+        hover_name="Isolate names",
+        color="SIR",
+        opacity=0.7,
+        title="Isolate MIC-values for different antibiotics",
+        range_y=[-11, 12],
+        template="plotly_dark",
+        hover_data={
+            "Antibiotics": False,
+            "Log2(MIC-value)": False,
+            "Scale": False,
+            "Pathogen": True,
+            "MIC value": True,
+            "Fastidiousness": True,
+        },
+    )
+
+    # Changes the dot color depending on SIR category
+    def change_trace_color(trace):
+        print(trace)
+        if trace.name == "Resistant":
+            trace.update(marker_color="tomato")
+        elif trace.name == "Intermediate":
+            trace.update(marker_color="gold")
+        elif trace.name == "Sensitive":
+            trace.update(marker_color="limegreen")
+        else:
+            raise ValueError("Not a valid trace")
+
+    # Update dot color
+    fig.for_each_trace(change_trace_color)
+
+    add_rectangles_to_plot(fig, antibiotics)
+
+    # Add border to dots
+    fig.update_traces(marker=dict(line=dict(width=1, color="DarkSlateGrey")))
+
+    # Modify x-ticks, y-ticks, legend and title
+    fig.update_layout(
+        xaxis=dict(tickmode="array", tickvals=x_axis, ticktext=antibiotics),
+        yaxis=dict(
+            tickmode="array",
+            tickvals=[i for i in range(-10, 12)],
+            ticktext=y_axis_ticktext,
+        ),
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
+        title_x=0.5,
+    )
+    fig.write_html("first_figure.html", auto_open=True)
+    # fig.show()
+
+
+def main():
+    # Load files
+    chosen_isolates_list = pd.read_csv("Chosen_isolates.csv")
+    CIB = pd.ExcelFile("CIB_TF-data_AllIsolates_20230302.xlsx")
+    matrix_EU = pd.read_excel(CIB, "matrix EU")
+
+    # Rename a long name for plotting purposes
+    matrix_EU.rename(
+        columns={"Trimethoprim-sulfamethoxazole": "Trimeth-sulf"}, inplace=True
+    )
+
+    # Select isolates
+    chosen_isolates = extract_chosen_isolates(chosen_isolates_list, matrix_EU)
+
+    # List of antiiotic names
+    antibiotics = list(chosen_isolates.columns[3:])
+
+    # Extract all SIRs for an antibiotic.
+    chosen_isolates_SIR = extract_SIR(chosen_isolates, antibiotics)
+
+    # Remove the tuples that have None in their SIR data
+    filtered_chosen_isolates_SIR = filter_mic_values(chosen_isolates_SIR)
+
+    # Extract the mic-values of each isolate for each antibiotic.
+    mic_data = extract_mic_data(filtered_chosen_isolates_SIR, antibiotics)
+
+    # Create dataframe used for plotting
+    plot_df = create_plot_df(antibiotics, mic_data)
+
+    plotly_dotplot(plot_df, antibiotics)
+
+
+if __name__ == "__main__":
+    main()
--- score_calc_functions.py
+++ score_calc_functions.py
@@ -5,7 +5,8 @@ from spread_score_functions import score_spread_list
 
 def concentration_to_index(concentration: float, abs_lowest_concentration: int):
     """Converts the concentration to a list index value"""
-    return int(np.log2(concentration) + abs_lowest_concentration)
+    # print(concentration)
+    return int(np.log2(float(concentration)) + abs_lowest_concentration)
 
 
 def extract_panel_data(panel: Panel):
@@ -27,25 +28,32 @@ def extract_panel_data(panel: Panel):
 def calc_spread_score(panel_data: dict, concentration_ranges: dict):
     """Calculates the spread score"""
     spread_list_score = 0
-    spread_dict = {}
     abs_lowest_concentration = np.abs(
         np.log2(concentration_ranges["Lowest concentration"])
     )
-    for abx, MIC_SIR in panel_data:
+    for abx, MIC_SIR in panel_data.items():
+        # Create a set of the unique mic values and remove off-scale values, denoted as None
+        unique_mic_values = {MIC for MIC, _ in MIC_SIR if MIC is not None}
+
         # Get the minimum and maximum on-scale concentration value for the antibiotic
         minimum_concentration = concentration_ranges[abx]["Lower"]
         maximum_concentration = concentration_ranges[abx]["Upper"]
 
         # Convert the concentration values to indices
-        minimum_concentration_index = concentration_to_index(
-            minimum_concentration, abs_lowest_concentration
-        )
-        maximum_concentration_index = concentration_to_index(
-            maximum_concentration, abs_lowest_concentration
-        )
-
-        # Create a set of the unique mic values
-        unique_mic_values = set(MIC for MIC in MIC_SIR)
+        if minimum_concentration == "Min_C":
+            minimum_concentration_index = 0
+        else:
+            minimum_concentration_index = concentration_to_index(
+                minimum_concentration, abs_lowest_concentration
+            )
+        if maximum_concentration == "Max_C":
+            maximum_concentration_index = concentration_ranges[
+                "Concentration range length"
+            ]
+        else:
+            maximum_concentration_index = concentration_to_index(
+                maximum_concentration, abs_lowest_concentration
+            )
 
         # Create a list for each antibiotic to display the spread of values
         spread_list = [
@@ -53,7 +61,8 @@ def calc_spread_score(panel_data: dict, concentration_ranges: dict):
         ]
 
         # Set all off-scale values to None in the spread_list
-        for index in range(len(spread_dict[abx])):
+        # Behöver hantera MAX_C och MIN_C
+        for index in range(len(spread_list)):
             if (
                 index < minimum_concentration_index
                 or index > maximum_concentration_index
@@ -66,6 +75,8 @@ def calc_spread_score(panel_data: dict, concentration_ranges: dict):
             )
             spread_list[mic_value_index] = 1
         spread_list_score += score_spread_list(spread_list)
+    if len(panel_data) == 0:
+        return 0
     return spread_list_score / len(panel_data)
 
 
@@ -81,10 +92,12 @@ def calc_coverage_score(panel_data: dict):
             if category not in panel_SIRs:
                 sir_coverage -= penalty
         coverage_score += coverage * sir_coverage
+    if len(panel_data) == 0:
+        return 0
     return coverage_score / len(panel_data)
 
 
-def calc_redundancy_score(panel_data: dict, redundancy_threshold=1):
+def calc_redundancy_score(panel_data: dict, redundancy_threshold: int):
     """Calculates the redundancy score"""
     number_of_MICS = 0
     number_of_redundant_MICS = 0
@@ -103,13 +116,13 @@ def calc_redundancy_score(panel_data: dict, redundancy_threshold=1):
     return number_of_redundant_MICS / number_of_MICS
 
 
-def calc_scores(panel: Panel, concentration_ranges: dict):
+def calc_scores(panel: Panel, concentration_ranges: dict, redundancy_threshold: int):
     """Calculates spread, coverage and redundancy score for entire panel"""
     antibiotic_mic = extract_panel_data(panel)
     scores = (
         calc_spread_score(antibiotic_mic, concentration_ranges),
         calc_coverage_score(antibiotic_mic),
-        calc_redundancy_score(antibiotic_mic),
+        calc_redundancy_score(antibiotic_mic, redundancy_threshold),
     )
 
     return scores
