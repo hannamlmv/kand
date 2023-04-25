cc235a13c21002326ce97bdb072cb97703706ba6
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-25 14:15:42 +0200

    Added a function that measures the runtime. Used as a decorator for the main-function. Added code to convert the chosen_isolates attribute in the panel_class to a csv file which can be used to plot the data points.

--- main.py
+++ main.py
@@ -1,37 +1,62 @@
 import json
 import pandas as pd
 import numpy as np
-from isolate_class import create_isolate_list
 from panel_class import Panel
+from isolate_class import create_isolate_list
 from add_isolate_functions import add_isolate
 
+import time
 
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Elapsed time: {end_time - start_time:.3f} seconds")
+        return result
+
+    return wrapper
+
+
+@measure_time
 def main():
+    file_path = "Chosen_isolates.csv"
+
     # Read in data from excel
     CIB = pd.ExcelFile("Q-linea_files\CIB_TF-data_AllIsolates_20230302.xlsx")
     matrix_EU = pd.read_excel(CIB, "matrix EU")
     antibiotic_concentration_ranges = json.load(open("abx_ranges.json"))
 
     # Create tuple of antibiotic names
-    antibiotics = tuple(matrix_EU.columns[3:])
+    # antibiotics = tuple(matrix_EU.columns[3:])
 
     # Initiate variables
     panel = Panel()
-    all_isolates = create_isolate_list(matrix_EU, antibiotics)
-    n = 50
+    all_isolates = create_isolate_list(matrix_EU)
+    redundancy_threshold = 1
+    number_of_isolates = 50
     spread_score_coeff = 1
     coverage_score_coeff = 1
     redundancy_score_coeff = 1
-    number_of_isolates_coeff = 0.01
+    # number_of_isolates_coeff = 0.01
     hyperparameters = np.array(
         (
             spread_score_coeff,
             coverage_score_coeff,
             redundancy_score_coeff,
-            number_of_isolates_coeff,
         )
     )
-    add_isolate(n, all_isolates, panel, hyperparameters)
+    add_isolate(
+        number_of_isolates,
+        all_isolates,
+        panel,
+        hyperparameters,
+        antibiotic_concentration_ranges,
+        redundancy_threshold,
+    )
+
+    panel.to_csv(file_path)
 
 
 if __name__ == "__main__":
