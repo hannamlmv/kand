b08191d109d0962ebbbc09672db6dfa3d7f3a458
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-27 16:45:28 +0200

    Added pathogen to isolate class

--- Classes/isolate_class.py
+++ Classes/isolate_class.py
@@ -6,8 +6,9 @@ from help_functions.extract_and_parse import (
 
 
 class Isolate:
-    def __init__(self, isolate_name: str, isolate_data: dict) -> None:
+    def __init__(self, isolate_name: str, pathogen: str, isolate_data: dict) -> None:
         self.name = isolate_name
+        self.pathogen = pathogen
         self.data = isolate_data
 
     def __str__(self) -> str:
@@ -19,6 +20,9 @@ class Isolate:
     def get_name(self):
         return self.name
 
+    def get_pathogen(self):
+        return self.pathogen
+
     def get_data(self):
         return self.data
 
@@ -26,12 +30,12 @@ class Isolate:
 def create_isolate_list(matrix_EU: pd.DataFrame) -> list[Isolate]:
     all_isolates = []
     for _, row in matrix_EU.iterrows():
-        isolate, antibiotic_data = row[0], list(row[3:].items())
+        isolate, pathogen, antibiotic_data = row[0], row[1], list(row[3:].items())
         isolate_data = {}
         # Store the MIC value and SIR data for all antibiotics that are not nip or missing bp
         for antibiotic, data in antibiotic_data:
             if parse_antibiotic_data(data):
                 isolate_data[antibiotic] = extract_antibiotic_data(data)
         # Add an isolate object to all_isolates with the current isolate name and isolate_data
-        all_isolates.append(Isolate(isolate, isolate_data))
+        all_isolates.append(Isolate(isolate, pathogen, isolate_data))
     return all_isolates
