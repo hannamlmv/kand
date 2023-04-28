0aa7acc0eab1fdc18b3e32fd8d90c5bedb10398c
Hanna Malmvall <hanna.malmvall@gmail.com>
2023-04-28 11:42:26 +0200

    spread_per_antibiotic klar

--- help_functions/score_calc_functions.py
+++ help_functions/score_calc_functions.py
@@ -31,7 +31,7 @@ def calc_spread_score(
     # TODO: Clean this function up. Very messy atm. Does too many things. concentration conversion should most likely
     # be handled in own function entirely instead of partly, which it is now.
     """Calculates the spread score"""
-    spread_list_score = 0
+    total_spread_list_score = 0
     abs_lowest_concentration = np.abs(
         np.log2(concentration_ranges["Lowest concentration"])
     )
@@ -79,12 +79,12 @@ def calc_spread_score(
                 mic_value, abs_lowest_concentration
             )
             spread_list[mic_value_index] = 1
-        spread_list_score += score_spread_list(spread_list)
+        total_spread_list_score += score_spread_list(spread_list)
         if per_antibiotic:
-            spread_per_antibiotic[abx] = spread_list_score
+            spread_per_antibiotic[abx] = score_spread_list(spread_list)
     if per_antibiotic:
         return spread_per_antibiotic
-    return spread_list_score / number_of_antibiotics
+    return total_spread_list_score / number_of_antibiotics
 
 
 def calc_coverage_score_old(
