ab75263162b8f8567d45257d170564dde3590fbe
vjhwong <victor.ju.hin.wong@gmail.com>
2023-04-28 11:23:22 +0200

    moved spread plot to own folder

---
+++ Visualisation/Spread_plot/spread_plot.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from Spread_plot.spread_plot_functions import *
+
+
+def main(chosen_isolates_list=None):
+    # Load files
+    if chosen_isolates_list is None:
+        chosen_isolates_list = pd.read_csv("Chosen_isolates_1.csv")
+    CIB = pd.ExcelFile("Q-linea_files/CIB_TF-data_AllIsolates_20230302.xlsx")
+    matrix_EU = pd.read_excel(CIB, "matrix EU")
+
+    # Rename a long name for plotting purposes
+    matrix_EU.rename(
+        columns={"Trimethoprim-sulfamethoxazole": "Trimeth-sulf"}, inplace=True
+    )
+
+    # Select isolates
+    chosen_isolates = extract_chosen_isolates(chosen_isolates_list, matrix_EU)
+
+    # List of antiiotic names
+    antibiotics = list(chosen_isolates.columns[3:])
+
+    # Extract all SIRs for an antibiotic.
+    chosen_isolates_SIR = extract_SIR(chosen_isolates, antibiotics)
+
+    # Remove the tuples that have None in their SIR data
+    filtered_chosen_isolates_SIR = filter_mic_values(chosen_isolates_SIR)
+
+    # Extract the mic-values of each isolate for each antibiotic.
+    mic_data = extract_mic_data(filtered_chosen_isolates_SIR, antibiotics)
+
+    # Create dataframe used for plotting
+    plot_df = create_plot_df(antibiotics, mic_data)
+
+    plotly_dotplot(plot_df, antibiotics)
+
+
+if __name__ == "__main__":
+    main()
