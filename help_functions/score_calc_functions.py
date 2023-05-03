daaa17ce1f3a0d97f183a35874acc85c2eae72c1
Hanna Malmvall <hanna.malmvall@gmail.com>
2023-05-03 10:23:59 +0200

    fixat coverage_score, tog bort den gamla

--- help_functions/score_calc_functions.py
+++ help_functions/score_calc_functions.py
@@ -93,37 +93,21 @@ def calc_spread_score(
         return spread_per_antibiotic
     return total_spread_list_score / number_of_antibiotics
 
-
-def calc_coverage_score_old(
-    panel_data: dict, number_of_antibiotics: int, coverage_penalties: dict
-):
-    """Calculates the coverage score"""
-    coverage_score = 0
-    for MIC_SIR in panel_data.values():
-        panel_SIRs = [SIR for MIC, SIR in MIC_SIR if MIC is not None]
-        number_of_mics = len(panel_SIRs)
-        coverage = min(1, 0.2 * number_of_mics)
-        sir_coverage = 1
-        for category, penalty in coverage_penalties.items():
-            if category not in panel_SIRs:
-                sir_coverage -= penalty
-        coverage_score += coverage * sir_coverage
-    return coverage_score / number_of_antibiotics
-
-
 def calc_coverage_score(
     panel_data: dict, number_of_antibiotics: int, coverage_demands: dict, coverage_total: int
 ):
     """Calculates the coverage score"""
-    coverage_sir_score = 0
+    coverage_score = 0
     for mic_sir in panel_data.values():
         coverage_counter = {key:0 for key in coverage_demands}
         for _, sir in mic_sir:
             coverage_counter[sir] += 1
         for sir in coverage_counter:
             coverage_counter[sir] = min(coverage_counter[sir], coverage_demands[sir])
-        coverage_sir_score += sum(coverage_counter.values())/sum(coverage_demands.values())
-    return coverage_sir_score / number_of_antibiotics
+        coverage_sir_score = sum(coverage_counter.values())/sum(coverage_demands.values())
+        total_coverage = min(1, 1/coverage_total * len(mic_sir))
+        coverage_score += coverage_sir_score * total_coverage
+    return coverage_score / number_of_antibiotics
 
 
 def calc_redundancy_score(panel_data: dict, redundancy_threshold: int):
