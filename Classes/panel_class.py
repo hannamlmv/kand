51b99df927a3510c535c6b2169efab07429fc424
vjhwong <victor.ju.hin.wong@gmail.com>
2023-05-05 13:51:14 +0200

    Added doc string

--- Classes/panel_class.py
+++ Classes/panel_class.py
@@ -8,8 +8,10 @@ Author: Victor Wong and Hanna Malmvall
 import pandas as pd
 from Classes.isolate_class import Isolate
 
+
 class Panel:
-    """ A Panel Class. """
+    """A Panel Class."""
+
     def __init__(
         self,
         number_of_antibiotics: int,
@@ -18,7 +20,7 @@ class Panel:
         coverage_score: float = 0,
         redundancy_score: float = 0,
     ) -> None:
-        """ Initialize a Panel object. """
+        """Initialize a Panel object."""
         self.chosen_isolates = chosen_isolates
         self.spread_score = spread_score
         self.coverage_score = coverage_score
@@ -31,54 +33,55 @@ class Panel:
 
     ### Getter methods ###
     def get_chosen_isolates(self) -> list[Isolate]:
-        """ Returns the list of Isolate objects currently in the Panel. """
+        """Returns the list of Isolate objects currently in the Panel."""
         return self.chosen_isolates
 
     def get_number_of_isolates(self) -> int:
-        """ Returns the number of Isolate objects which are in the Panel. """
+        """Returns the number of Isolate objects which are in the Panel."""
         return len(self.chosen_isolates)
 
     def get_spread_score(self) -> float:
-        """ Returns the spread score of the Panel object. """
+        """Returns the spread score of the Panel object."""
         return self.spread_score
 
     def get_coverage_score(self) -> float:
-        """ Returns the coverage score of the Panel object. """
+        """Returns the coverage score of the Panel object."""
         return self.coverage_score
 
     def get_redundancy_score(self) -> float:
-        """ Returns the redundancy score of the Panel object. """
+        """Returns the redundancy score of the Panel object."""
         return self.redundancy_score
 
     def get_number_antibiotics(self) -> int:
-        """ Returns number of antibiotics of the Panel object. """
+        """Returns number of antibiotics of the Panel object."""
         return self.number_of_antibiotics
 
     def get_all_isolate_names(self) -> list[str]:
-        """ Returns a list of names for all isolates in the Panel object. """
+        """Returns a list of names for all isolates in the Panel object."""
         return [isolate.get_name() for isolate in self.chosen_isolates]
 
     def get_all_isolate_data(self) -> dict:
-        """ Returns a dictionary with isolate names and their data 
-        for all isolates in the Panel object. """
+        """Returns a dictionary with isolate names and their data
+        for all isolates in the Panel object."""
         return {
             isolate.get_name(): isolate.get_data() for isolate in self.chosen_isolates
         }
 
     ### Methods to add and remove isolates from the panel ###
     def append_isolate(self, isolate) -> None:
-        """ Adds an isolate to the list of chosen isolates. """
+        """Adds an isolate to the list of chosen isolates."""
         self.chosen_isolates.append(isolate)
 
     def remove_isolate(self, isolate) -> None:
-        """ Removes an isolate to the list of chosen isolates. """
+        """Removes an isolate to the list of chosen isolates."""
         self.chosen_isolates.remove(isolate)
 
     ### Other methods ###
     def to_csv(self, file_path: str):
-        
+        """Convert the isolates in the panel to a csv file"""
         df = pd.DataFrame(self.get_all_isolate_names(), columns=["Isolate"])
         df.to_csv(file_path, index=False)
 
     def to_DataFrame(self):
+        """Convert the isolates in the panel to a pandas DataFrame"""
         return pd.DataFrame(self.get_all_isolate_names(), columns=["Isolate"])
